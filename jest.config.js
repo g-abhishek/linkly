c58d08428e08aa903eba1ea1b9caf2c4c732fd3c
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-05-17 01:02:29 +0530

    formatting

--- jest.config.js
+++ jest.config.js
@@ -5,23 +5,21 @@
 
 /** @type {import('jest').Config} */
 const config = {
-  // The glob patterns Jest uses to detect test files
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-
-  // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ["/node_modules/"],
-
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
-
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"], // The glob patterns Jest uses to detect test files
+  coverageDirectory: "coverage", // The directory where Jest should output its coverage files
+  coveragePathIgnorePatterns: ["/node_modules/"], // An array of regexp pattern strings used to skip coverage collection
+  coverageProvider: "v8", // Indicates which provider should be used to instrument code for coverage
+  clearMocks: true, // Automatically clear mock calls, instances, contexts and results before every test
+  collectCoverage: true, // Indicates whether the coverage information should be collected while executing the test
+  // An object that configures minimum threshold enforcement for coverage results
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -42,16 +40,6 @@ const config = {
   //   "lcov",
   //   "clover"
   // ],
-
-  // An object that configures minimum threshold enforcement for coverage results
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
