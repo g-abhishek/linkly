873d6599502669c1cbdc203e481ab0a5d3479b6c
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-05-17 14:35:26 +0530

    added prettier formatting with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,16 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: "standard",
+  extends: [
+    "standard",
+    "plugin:prettier/recommended", // includes both eslint-plugin-prettier and eslint-config-prettier // "eslint-plugin-prettier" runs Prettier formatting checks as ESLint rules and reports formatting issues. // "eslint-config-prettier" disables ESLint rules that clash with Prettier.
+  ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [
-        ".eslintrc.{js,cjs}",
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
         sourceType: "script",
       },
@@ -23,11 +24,13 @@ module.exports = {
     ecmaVersion: "latest",
   },
   rules: {
-    "no-unused-vars": "error",
-    quotes: ["error", "double"], // 👈 force double quotes
-    semi: ["error", "always"], // 👈 Enforce semicolons
-    "comma-dangle": ["error", "always-multiline"], // 👈 Enforce trailing commas
-    "space-before-function-paren": ["error", "never"], // 👈 Enforce no space before ()
-    'indent': ['error', 2], // 👈 Enforce 2 spaces for indentation
+    "no-unused-vars": "error", // keep code-quality rules only here
+
+    // Below are redundent now, as we are using prettier for formatting
+    // quotes: ["error", "double"], // 👈 force double quotes
+    // semi: ["error", "always"], // 👈 Enforce semicolons
+    // "comma-dangle": ["error", "always-multiline"], // 👈 Enforce trailing commas
+    // "space-before-function-paren": ["error", "never"], // 👈 Enforce no space before ()
+    // 'indent': ['error', 2], // 👈 Enforce 2 spaces for indentation
   },
 };
