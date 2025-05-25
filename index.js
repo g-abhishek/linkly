641dd7b29169a457acca52661c845c416502df71
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-05-25 21:50:16 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const config = require("./config");
-require("./connections/postgres.init");
 const getMainServer = require("./app/server");
 
 const PORT = config.get("port");
--- run.test.sh
+++ run.test.sh
@@ -1,4 +1,7 @@
 # export PORT=3000
 export NODE_ENV=test
+export POSTGRES_DB_URL="postgresql://root:root@0.0.0.0:5434/linkly-test"
+export REDIS_DB_URL="redis://127.0.0.1:6379/0"
 
+npm run migrate
 npm run test
